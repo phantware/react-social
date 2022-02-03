df15327e4167aedc9ee06b979dcd7e28a14113fe
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-03 09:54:44 +0100

    sidebar componenet implemented

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,7 @@
+import './feed.css'
+
+const Feed = () => {
+  return <div className='feed'>Feeds</div>
+}
+
+export default Feed
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,19 @@
 import Topbar from '../../components/tobar/Topbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Feed from '../../components/feed/Feed'
+import Rightbar from '../../components/rightbar/Rightbar'
 import './home.css'
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Topbar />
-    </div>
+      <div className='homeContainer'>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
   )
 }
 
