f9c8fc114936ebbb65327affc22729e71794530a
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-07 19:05:53 +0100

    router path added to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import Profile from './pages/profile/Profile'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/register'>
+          <Register />
+        </Route>
+        <Route path='/profile/:username'>
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
