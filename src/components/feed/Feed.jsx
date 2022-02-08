cb2491017d6b90369fd422432d7bf194ac49c7f7
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-08 03:37:55 +0100

    Proxy added to package.json

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,17 +1,27 @@
+import React, { useState, useEffect } from 'react'
 import Post from '../post/Post'
 import Share from '../share/Share'
 import './feed.css'
-import { Posts } from '../../dummyData'
+import axios from 'axios'
 
 const Feed = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get('posts/timeline/61ff0e08a71109eb703bc660')
+      console.log(res)
+    }
+    fetchPosts()
+  }, [])
   return (
     <div className='feed'>
       <div className='feedWrapper'>
         <Share />
-        {Posts.map((p) => {
+        {/* {Posts.map((p) => {
           const { id } = p
           return <Post key={id} post={p} />
-        })}
+        })} */}
       </div>
     </div>
   )
