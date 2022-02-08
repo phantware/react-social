0fa9d854dce5d3f3186ef1a05c7fa7112b36d771
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-08 12:40:58 +0100

    apiCalls folder created and login logics implemented

---
+++ src/apiCalls.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+export const loginCall = async (userCredentials, dispatch) => {
+  dispatch({ type: 'LOGIN_START' })
+  try {
+    const res = await axios.post('auth/login', userCredentials)
+    dispatch({ typ: 'LOGIN_SUCCESSFUL', payload: res.data })
+  } catch (error) {
+    dispatch({ type: 'LOGIN_FAILURE', payload: error })
+  }
+}
