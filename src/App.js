64c14efc9c69e7e97a103ad1e8146fd2ea77e648
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-08 16:12:31 +0100

    Login logic implemented

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Register from './pages/register/Register'
 import Profile from './pages/profile/Profile'
-
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from './context/AuthContext'
 
 const App = () => {
+  const { user } = useContext(AuthContext)
   return (
     <Router>
       <Switch>
         <Route exact path='/'>
-          <Home />
+          {user ? <Home /> : <Register />}
         </Route>
-        <Route path='/login'>
-          <Login />
-        </Route>
+        <Route path='/login'>{user ? <Redirect to='/' /> : <Login />}</Route>
         <Route path='/register'>
-          <Register />
+          {user ? <Redirect to='/' /> : <Register />}
         </Route>
         <Route path='/profile/:username'>
           <Profile />
