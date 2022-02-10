7469762eca5c44e61d373f2895df0305cc895b36
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-10 11:56:52 +0100

    apiCall updated

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-export const loginCall = async (userCredentials, dispatch) => {
+export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: 'LOGIN_START' })
   try {
-    const res = await axios.post('auth/login', userCredentials)
-    dispatch({ type: 'LOGIN_SUCCESSFUL', payload: res.data })
-  } catch (error) {
-    dispatch({ type: 'LOGIN_FAILURE', payload: error })
+    const res = await axios.post('/auth/login', userCredential)
+    dispatch({ type: 'LOGIN_SUCCESS', payload: res.data })
+  } catch (err) {
+    dispatch({ type: 'LOGIN_FAILURE', payload: err })
   }
 }
