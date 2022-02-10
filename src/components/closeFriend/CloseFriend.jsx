d6f380a3bf42151b5b2e72ce0ca980c35dc3d9ec
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-10 11:53:26 +0100

    closefriend updated

--- src/components/closeFriend/CloseFriend.jsx
+++ src/components/closeFriend/CloseFriend.jsx
@@ -1,13 +1,11 @@
 import './closeFriend.css'
 
-const CloseFriend = ({ user }) => {
+export default function CloseFriend({ user }) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER
   return (
     <li className='sidebarFriend'>
-      <img src={PF + user.profilePicture} alt='' className='sidebarFriendImg' />
+      <img className='sidebarFriendImg' src={PF + user.profilePicture} alt='' />
       <span className='sidebarFriendName'>{user.username}</span>
     </li>
   )
 }
-
-export default CloseFriend
