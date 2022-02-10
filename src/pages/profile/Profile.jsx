0f6ba3cd853e81b5c141610fb587b31f22d3a9fe
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-10 11:50:17 +0100

    profile updated

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,13 +1,13 @@
-import Topbar from '../../components/tobar/Topbar'
+import './profile.css'
+import Topbar from '../../components/topbar/Topbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Feed from '../../components/feed/Feed'
 import Rightbar from '../../components/rightbar/Rightbar'
-import './profile.css'
-import React, { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router'
 
-const Profile = () => {
+export default function Profile() {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER
   const [user, setUser] = useState({})
   const username = useParams().username
@@ -29,22 +29,22 @@ const Profile = () => {
           <div className='profileRightTop'>
             <div className='profileCover'>
               <img
+                className='profileCoverImg'
                 src={
                   user.coverPicture
                     ? PF + user.coverPicture
                     : PF + 'person/noCover.png'
                 }
                 alt=''
-                className='profileCoverImg'
               />
               <img
+                className='profileUserImg'
                 src={
                   user.profilePicture
                     ? PF + user.profilePicture
                     : PF + 'person/noAvatar.png'
                 }
                 alt=''
-                className='profileUserImg'
               />
             </div>
             <div className='profileInfo'>
@@ -61,5 +61,3 @@ const Profile = () => {
     </>
   )
 }
-
-export default Profile
