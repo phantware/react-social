c5ac1c63871e759b50739c1dfba6792cb2cb9302
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-10 11:52:07 +0100

    profile updated

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,5 +1,5 @@
 import './profile.css'
-import Topbar from '../../components/topbar/Topbar'
+import Topbar from '../../components/tobar/Topbar'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Feed from '../../components/feed/Feed'
 import Rightbar from '../../components/rightbar/Rightbar'
