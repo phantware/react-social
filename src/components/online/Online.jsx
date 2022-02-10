0f4381ec994ea8c8ef9de49254cb3ed51b79a30e
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-10 11:54:55 +0100

    online updated

--- src/components/online/Online.jsx
+++ src/components/online/Online.jsx
@@ -1,22 +1,19 @@
 import './online.css'
 
-const Online = ({ user }) => {
+export default function Online({ user }) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER
+
   return (
-    <div>
-      <li className='rightbarFriend'>
-        <div className='rightbarProfileImgContainer'>
-          <img
-            src={PF + user.profilePicture}
-            alt='person'
-            className='rightbarProfileImg'
-          />
-          <span className='rightbaronline'></span>
-        </div>
-        <span className='rightbarUsername'>{user.username}</span>
-      </li>
-    </div>
+    <li className='rightbarFriend'>
+      <div className='rightbarProfileImgContainer'>
+        <img
+          className='rightbarProfileImg'
+          src={PF + user.profilePicture}
+          alt=''
+        />
+        <span className='rightbarOnline'></span>
+      </div>
+      <span className='rightbarUsername'>{user.username}</span>
+    </li>
   )
 }
-
-export default Online
